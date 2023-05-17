e98996ddbbddc143da3f1572688536553f0ae9bd
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-05-17 12:13:00 -0300

    Arreglo conexion con la app biblioteca

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca.BibliotecaConfig'
+    'biblioteca'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'America/Argentina/Buenos Aires'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
