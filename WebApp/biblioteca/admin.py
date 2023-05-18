156a0c021d4087ca8c19ad363138913fe31a42db
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-05-18 20:12:34 -0300

    Modelo Empleado registrado en admin

--- WebApp/biblioteca/admin.py
+++ WebApp/biblioteca/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Autor, Empleado
 
 # Register your models here.
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    """Clase que define la configuración personalizada del panel de administración para el modelo Empleado."""
+    list_display = ["nombre", "apellido", "numero_legajo", "activo"]
+    search_fields = ["nombre", "apellido"]
+    list_filter = ["activo"]
