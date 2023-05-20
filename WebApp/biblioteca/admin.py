924a47c951cce511002ce1e2d97cd29ca78a642a
Hernan Fresco <herny.12@gmail.com>
2023-05-20 16:23:17 -0300

    libro-prestamo

--- WebApp/biblioteca/admin.py
+++ WebApp/biblioteca/admin.py
@@ -1,21 +1,40 @@
 from django.contrib import admin
 
-from biblioteca.models import Autor,Empleado,Prestamo
+from biblioteca.models import Autor,Empleado,Prestamo,Libro
 
 class AutorAdmin(admin.ModelAdmin):
     model = Autor
     list_display = ("nombre", "apellido", "nacionalidad", "activo")
     search_fields = ("nombre", "apellido")
     list_filter = ("activo", "nacionalidad")
-
-admin.site.register(Autor, AutorAdmin)
     
-
-
 class EmpleadoAdmin(admin.ModelAdmin):
     model= Empleado
-    list_display = "emp_nombre", "emp_apellido", "emp_legajo"
-    search_fields = ("emp_nombre", "emp_apellido", "emp_legajo")
+    list_display = "emp_nombre", "emp_apellido", "emp_legajo","emp_activo"
+    search_fields = "emp_nombre", "emp_apellido", "emp_legajo","emp_activo"
     list_filter = "emp_nombre", "emp_apellido","emp_legajo", "emp_activo"
 
+class LibroAdmin(admin.ModelAdmin):
+    """Configura los parametros con los que se va a mostrar un libro en el admin
+    """
+    model=Libro
+    list_display = "lib_titulo","lib_autor","lib_descripcion","lib_isbn","lib_activo"
+    search_fields = "lib_titulo","lib_descripcion","lib_isbn","lib_activo","lib_autor__nombre","lib_autor__apellido","lib_autor__nacionalidad"
+    list_filter = "lib_activo","lib_autor__nombre","lib_autor__apellido","lib_autor__nacionalidad"
+    
+
+class PrestamoAdmin(admin.ModelAdmin):
+    """Configura los parametros con los que se va a mostrar un Prestamo en el admin
+    """
+    model= Prestamo
+    list_display= 'pres_fecha','pres_devolucion','socio','libro','empleado'
+    search_fields= 'pres_fecha','pres_devolucion','socio__nombre','socio__apellido','libro__lib_titulo',
+    'libro__lib_isbn','libro__lib_autor__nombre','libro__lib_autor__apellido','libro__lib_autor__nacionalidad',
+    'empleado__emp_nombre','empleado__emp_apellido','empleado__emp_legajo'
+    list_filter= 'pres_fecha','pres_devolucion','empleado__emp_nombre','empleado__emp_apellido','empleado__emp_legajo'
+
 admin.site.register(Empleado,EmpleadoAdmin)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro,LibroAdmin)
+admin.site.register(Prestamo,PrestamoAdmin)
+
