55be9ac436b732e633aad89354e2fd7395aa07c8
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-05-22 21:34:45 -0300

    Correccion de template agregar empleados

--- WebApp/biblioteca/forms.py
+++ WebApp/biblioteca/forms.py
@@ -1,7 +1,4 @@
 from django import forms
 from .models import Empleado
 
-class EmpleadoForm(forms.ModelForm):
-    class Meta:
-        model = Empleado
-        fields = ["emp_nombre", "emp_apellido", "emp_legajo"]
+
