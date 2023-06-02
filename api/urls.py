41395c7911e559b1d148e232156dc0e7c936d3ca
rodrigonza92 <rodrigo.e.gonza@gmail.com>
2023-06-01 22:09:47 -0300

    Se crea endpoint de un libro por id

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('libros/', views.listado_libros_endpoint, name='api_listado_libros'),
+    path('libros/<int:id>', views.libro_por_id, name='api_libro_por_id')
 ]
